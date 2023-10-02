1020d4669e84d2093ce2fe33fc5cb089b66924b1
Alejandro Cortes <ale.crts1996@gmail.com>
2023-10-02 00:15:27 -0600

    Base app, routes and states.

---
+++ vue-search-meals/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col p-8 justify-center">
+    <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search meals" />
+
+    <div class="flex justify-center mt-2 gap-2">
+      <router-link v-for="letter of letters" :to="{name: 'byLetter', params: {letter}}" :key="letter" >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import store from '../store';
+
+const meals = computed(() => store.state.meals)
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZ'.split("")
+
+</script>
+
+<style scoped></style>
